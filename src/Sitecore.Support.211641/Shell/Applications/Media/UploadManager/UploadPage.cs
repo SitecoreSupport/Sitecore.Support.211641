92296d47b620d00272ca7491b3e5cd5865bc65d7
DK\mao <mao@sitecore.net>
2018-03-14 19:11:48 +0200

    Fix retireving private field issue

--- src/Sitecore.Support.211641/Shell/Applications/Media/UploadManager/UploadPage.cs
+++ src/Sitecore.Support.211641/Shell/Applications/Media/UploadManager/UploadPage.cs
@@ -1,7 +1,10 @@
 ﻿using Sitecore.Configuration;
 using Sitecore.Data;
+using Sitecore.Data.Items;
 using Sitecore.Diagnostics;
 using Sitecore.Globalization;
+using Sitecore.Pipelines;
+using Sitecore.Pipelines.Upload;
 using Sitecore.Shell.Web;
 using Sitecore.Text;
 using Sitecore.Web;
@@ -18,7 +21,8 @@ namespace Sitecore.Support.Shell.Applications.Media.UploadManager
     protected override void OnLoad(EventArgs e)
     {
       base.OnLoad(e);
-      if ((bool)this.GetType().GetField("requestLengthExceeded", BindingFlags.NonPublic).GetValue(this))
+      if ((bool)typeof(Sitecore.Shell.Applications.Media.UploadManager.UploadPage)
+        .GetField("requestLengthExceeded", BindingFlags.NonPublic | BindingFlags.Instance).GetValue(this))
       {
         string arg = Translate.Text("The file is too big to be uploaded.\n\nThe maximum size of a file that can be uploaded is {0}.", new object[]
         {
@@ -37,9 +41,9 @@ namespace Sitecore.Support.Shell.Applications.Media.UploadManager
         }));
         try
         {
-          this.GetType().GetMethod("UploadFiles", BindingFlags.NonPublic).Invoke(this, new object[] { listString });
+          this.UploadFiles(listString);
         }
-        catch (Exception ex)
+        catch
         {
           this.ErrorText.Value = Translate.Text("One or more files could not be uploaded.\n\nSee the Log file for more details.");
         }
@@ -67,6 +71,82 @@ namespace Sitecore.Support.Shell.Applications.Media.UploadManager
       if (!Settings.Upload.UserSelectableDestination)
       {
         this.AsFilesCell.Style["display"] = "none";
+      }
+    }
+
+    private void UploadFiles(ListString items)
+    {
+      Assert.ArgumentNotNull(items, "items");
+      if (base.Request.Files.Count > 0)
+      {
+        string @string = StringUtil.GetString(new string[]
+        {
+      base.Request.Form["Uri"]
+        });
+        bool overwrite = StringUtil.GetString(new string[]
+        {
+      base.Request.Form["Overwrite"]
+        }) == "1";
+        bool unpack = StringUtil.GetString(new string[]
+        {
+      base.Request.Form["Unpack"]
+        }) == "1";
+        bool versioned = StringUtil.GetString(new string[]
+        {
+      base.Request.Form["Versioned"]
+        }) == "1";
+        bool flag = StringUtil.GetString(new string[]
+        {
+      base.Request.Form["AsFiles"]
+        }) == "1";
+        string folder = string.Empty;
+        Language language = Sitecore.Context.ContentLanguage;
+        ItemUri itemUri = ItemUri.Parse(@string);
+        if (itemUri != null)
+        {
+          folder = itemUri.GetPathOrId();
+          language = itemUri.Language;
+        }
+        UploadArgs uploadArgs = new UploadArgs();
+        uploadArgs.Files = base.Request.Files;
+        uploadArgs.Folder = folder;
+        uploadArgs.Overwrite = overwrite;
+        uploadArgs.Unpack = unpack;
+        uploadArgs.Versioned = versioned;
+        uploadArgs.Language = language;
+        uploadArgs.CloseDialogOnEnd = false;
+        uploadArgs.Destination = (flag ? UploadDestination.File : UploadDestination.Database);
+        Pipeline pipeline = PipelineFactory.GetPipeline("uiUpload");
+        try
+        {
+          pipeline.Start(uploadArgs);
+        }
+        catch (Exception ex)
+        {
+          if (ex.InnerException is OutOfMemoryException)
+          {
+            uploadArgs.ErrorText = Translate.Text("A file is too big to be uploaded.\n\nThe maximum size of a file that can be uploaded is {0}.", new object[]
+            {
+          MainUtil.FormatSize(Settings.Media.MaxSizeInDatabase)
+            });
+          }
+          else
+          {
+            uploadArgs.ErrorText = "An error occured while uploading:\n\n" + ex.InnerException;
+          }
+        }
+        if (!string.IsNullOrEmpty(uploadArgs.ErrorText))
+        {
+          this.ErrorText.Value = uploadArgs.ErrorText;
+        }
+        foreach (Item current in uploadArgs.UploadedItems)
+        {
+          string text = current.Uri.ToString();
+          if (!items.Contains(text))
+          {
+            items.Add(text);
+          }
+        }
       }
     }
   }
